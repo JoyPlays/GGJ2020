f8b8a4d83d6e5c165f822236a1255cb4d3d2bb48
Jānis Gitendorfs <janis.gitendorfs@gmail.com>
2020-02-01 00:36:52 +0200

    Game object replacer component
    Game object replacer component

---
+++ Assets/Ugitty/Components/GameObjectReplace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Ugitty.Components
+{
+    public class GameObjectReplace : MonoBehaviour
+    {
+        public GameObject initialObject;
+        public GameObject replaceObject;
+
+        private bool hasReplaced = false;
+
+        public void Replace()
+        {
+            if (hasReplaced) return;
+
+            // Remove old object
+            Destroy(initialObject);
+
+            // Add new object
+            Instantiate(replaceObject, initialObject.transform.position, initialObject.transform.rotation);
+
+            hasReplaced = true;
+        }
+    }
+}
