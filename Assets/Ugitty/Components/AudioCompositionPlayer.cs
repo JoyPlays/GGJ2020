51274b02ce1ee8c2252e3e77c048b27d35b43e3e
Jānis Gitendorfs <janis.gitendorfs@gmail.com>
2020-01-31 23:45:50 +0200

    Audio composition player
    Audio composition player

---
+++ Assets/Ugitty/Components/AudioCompositionPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ugitty.Audio;
+
+namespace Ugitty.Components
+{
+    public class AudioCompositionPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+        [SerializeField]
+        private AudioComposition composition;
+
+        public void Play()
+        {
+            composition.Play(audioSource);
+        }
+    }
+}
---
+++ Assets/Ugitty/Editor/AudioCompositionEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace Ugitty.Audio
+{
+	[CustomEditor(typeof(AudioComposition), true)]
+	public class AudioEventEditor : Editor
+	{
+
+		[SerializeField] private AudioSource _previewer;
+
+		public void OnEnable()
+		{
+			_previewer = EditorUtility.CreateGameObjectWithHideFlags("Audio preview", HideFlags.HideAndDontSave, typeof(AudioSource)).GetComponent<AudioSource>();
+		}
+
+		public void OnDisable()
+		{
+			DestroyImmediate(_previewer.gameObject);
+		}
+
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+			if (GUILayout.Button("Preview"))
+			{
+				((AudioComposition)target).Play(_previewer);
+			}
+			EditorGUI.EndDisabledGroup();
+		}
+	}
+}
