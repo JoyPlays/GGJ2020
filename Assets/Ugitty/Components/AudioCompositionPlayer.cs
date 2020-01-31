a96fd807d15ea603fb70d32715e68ee57c681cde
Jānis Gitendorfs <janis.gitendorfs@gmail.com>
2020-01-31 23:53:21 +0200

    Update AudioCompositionPlayer.cs
    Added additional checks for Audio Composition player

--- Assets/Ugitty/Components/AudioCompositionPlayer.cs
+++ Assets/Ugitty/Components/AudioCompositionPlayer.cs
@@ -1,19 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Ugitty.Audio;
 
 namespace Ugitty.Components
 {
     public class AudioCompositionPlayer : MonoBehaviour
     {
-        [SerializeField]
-        private AudioSource audioSource;
-        [SerializeField]
-        private AudioComposition composition;
+        public AudioSource audioSource;
+        public AudioComposition composition;
 
         public void Play()
         {
+            if (audioSource == null)
+            {
+                Debug.LogError("Missing audio source for " + this.name);
+                return;
+            }
+
+            if (composition == null)
+            {
+                Debug.LogWarning("Missing audio composition for " + this.name);
+                return;
+            }
+
             composition.Play(audioSource);
         }
     }
