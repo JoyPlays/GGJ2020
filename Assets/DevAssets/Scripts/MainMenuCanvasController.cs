3b05ffc1c3f915bbc6059f8f3334071162e89713
George <thebrainycraft@gmail.com>
2020-02-01 17:54:56 +0200

    Added MainMenuCanvasController.cs and its prefab, TextMesh Pro.

---
+++ Assets/DevAssets/Scripts/MainMenuCanvasController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCanvasController : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu = null;
+    [SerializeField] GameObject creditsMenu = null;
+
+    public void StartGame ()
+    {
+        print("Begin game");
+    }
+
+    public void EnableCreditsMenu()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void DisableCreditsMenu()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void Quit ()
+    {
+        print("Quit game");
+        Application.Quit();
+    }
+}
