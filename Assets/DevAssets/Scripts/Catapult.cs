d712efea8873cec32fd4313e4c5abb483f95c90f
Joy <davis.toliasvili@gmail.com>
2020-02-01 17:05:16 +0200

    comit yo

---
+++ Assets/DevAssets/Scripts/Catapult.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+	[Header("Projectile Setup")]
+	[SerializeField]
+	private Projectile projectile;
+	private Rigidbody projectilerb;
+	[SerializeField]
+	private Transform projectilePosition;
+	[SerializeField]
+	private bool catapultLoaded;
+	[SerializeField]
+	private float projectileSpeed = 10f;
+
+	public void ReloadCatapult()
+	{
+		if (!catapultLoaded)
+		{
+			GameObject pr = Instantiate(projectile, projectilePosition).gameObject;
+			projectilerb = pr.GetComponent<Rigidbody>();
+			pr.transform.localPosition = Vector3.zero;
+			pr.transform.rotation = Quaternion.identity;
+			catapultLoaded = true;
+		}
+	}
+
+	public void ReleaseProjectile()
+	{
+		catapultLoaded = false;
+		projectilerb.isKinematic = false;
+		projectilerb.angularVelocity = projectilePosition.transform.forward * projectileSpeed;
+	}
+}
---
+++ Assets/DevAssets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public GameObject explosionParticle;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision.collider.name);
+		GameObject explosionEffect = Instantiate(explosionParticle, transform.position, Quaternion.identity);
+		Destroy(explosionEffect, 3f);
+		Destroy(gameObject);
+	}
+}
