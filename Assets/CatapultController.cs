41db7746092628226eb6aaad9a241838e289e270
George <thebrainycraft@gmail.com>
2020-02-01 23:26:35 +0200

    Added CatapultController.cs.

---
+++ Assets/CatapultController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultController : MonoBehaviour
+{
+    [SerializeField] Animator[] catapults = null;
+
+    IEnumerator Load()
+    {
+        foreach (Animator a in catapults)
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 0.5f));
+            a.SetBool("load", true);
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        foreach (Animator a in catapults)
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 0.5f));
+            a.SetBool("shoot", true);
+        }
+    }
+}
