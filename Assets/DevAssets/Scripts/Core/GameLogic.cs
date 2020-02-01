eb6c42b77f5bb53f9895d834cf892e91872fca7e
Igors Boldovs <toag.iggy@gmail.com>
2020-02-01 11:57:48 +0200

    *some entity stuff

--- Assets/DevAssets/Scripts/Core/GameLogic.cs
+++ Assets/DevAssets/Scripts/Core/GameLogic.cs
@@ -1,9 +1,53 @@
-﻿using UnityEngine;
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
+    public GameObject zombiePrefab;
+
+    public Transform zombieSpawnPoint;
+    public Transform zombieEndPoint;
+    
+    private Entity zombieEntityPrefab;
+    private EntityManager manager;
+    
     private void Awake()
     {
         The.gameLogic = this;
+        
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, new BlobAssetStore());
+        zombieEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(zombiePrefab, settings);
+        
+        SpawnZombies();
+    }
+    
+    void SpawnZombies()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Entity zombie = manager.Instantiate(zombieEntityPrefab);
+            Vector3 dir = (zombieEndPoint.position - zombieSpawnPoint.position).normalized;
+            Quaternion rot = Quaternion.LookRotation(dir);
+            manager.SetComponentData(zombie, new Translation{Value = zombieSpawnPoint.position});
+            manager.SetComponentData(zombie, new Rotation{Value = rot});
+            manager.AddComponentData(zombie, new ZombieTagComponent());
+
+            // Vector3 dir = (zombieEndPoint.position - zombieSpawnPoint.position).normalized;
+            // Vector3 speed = dir * 1f;
+
+
+            // PhysicsVelocity velocity = new PhysicsVelocity()
+            // {
+            //     Linear = speed,
+            //     Angular = float3.zero
+            // };
+            //
+            // manager.AddComponentData(zombie, velocity);
+        }
     }
 }
---
+++ Assets/DevAssets/Scripts/Systems/TurnTowardsEndPointSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+[UpdateAfter(typeof(MoveForwardComponent))]
+public class TurnTowardsEndPointSystem : JobComponentSystem
+{
+    [RequireComponentTag(typeof(ZombieTagComponent))]
+    struct TurnJob : IJobForEach <Translation, Rotation>
+    {
+        public float3 endPointPosition;
+        
+        public void Execute(ref Translation pos, ref Rotation rot)
+        {
+            float3 heading = endPointPosition - pos.Value;
+            heading.y = 0f;
+            rot.Value = quaternion.LookRotation(heading, math.up());
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new TurnJob
+        {
+            endPointPosition = The.gameLogic.zombieEndPoint.position
+        };
+
+        return job.Schedule(this, inputDeps);
+    }
+}
