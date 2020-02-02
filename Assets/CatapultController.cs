b9a1b51db080980b7f34d72c5480e612d7068859
Davis (Joy) <davis.toliasvili@gmail.com>
2020-02-02 13:44:33 +0200

    wtf push

--- Assets/CatapultController.cs
+++ Assets/CatapultController.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class CatapultController : MonoBehaviour
 {
     [SerializeField] Animator[] catapults = null;
+	private bool shooting = false;
 
-    IEnumerator Load()
+	private void Update()
+	{
+		if (!shooting)
+		{
+			StartCoroutine("Load");
+		}
+		
+	}
+
+	IEnumerator Load()
     {
+		shooting = true;
         foreach (Animator a in catapults)
         {
             yield return new WaitForSeconds(Random.Range(0f, 0.5f));
             a.SetBool("load", true);
         }
+		yield return new WaitForSeconds(3f);
+		StartCoroutine("Shoot");
     }
 
     IEnumerator Shoot()
@@ -22,5 +35,7 @@ public class CatapultController : MonoBehaviour
             yield return new WaitForSeconds(Random.Range(0f, 0.5f));
             a.SetBool("shoot", true);
         }
+		yield return new WaitForSeconds(3f);
+		shooting = false;
     }
 }
